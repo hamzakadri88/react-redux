b0e1f3e2d553103a3f0ad19373a9e3129a7db4f0
Kadri Hamza <hamza.kadri@metis-innovations.com>
2019-10-06 13:15:30 +0100

    Adding routing for pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import React from 'react';
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       Application React
-      <Home />
-      <About />
-      <Contact />
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </BrowserRouter>
     </div>
   );
 }
